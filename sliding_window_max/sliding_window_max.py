0dd1ef84391811e5c0795cd1c9837650872c4597
Tyler Christian <tylerc71197@gmail.com>
2020-05-21 16:51:07 -0400

    MVP

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,21 +2,38 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+import collections
+
+
 def sliding_window_max(nums, k):
+    deque, result = collections.deque(), []
+
+    for index, num in enumerate(nums):
+        while deque and deque[0][1] <= index - k:
+            deque.popleft()
+        while deque and num > deque[-1][0]:
+            deque.pop()
+        deque.append([num, index])
+
+        if index >= k - 1:
+            result.append(deque[0][0])
+
+    return result
+
+    # old way much slower 
     # Your code here
-    final_array = []
-    array_max = None
-    for index, i in enumerate(nums):
-        window = nums[index:index + k]
-        if len(window) < k:
-            pass
-        else:
-            array_max = max(window)
-            final_array.append(array_max)
-
-    return final_array
-
-
+    # final_array = []
+    # array_max = None
+    # for index, i in enumerate(nums):
+    #     window = nums[index:index + k]
+    #     if len(window) < k:
+    #         pass
+    #     else:
+    #         array_max = max(window)
+    #         final_array.append(array_max)
+    #
+    # return final_array
 
 
 if __name__ == '__main__':
